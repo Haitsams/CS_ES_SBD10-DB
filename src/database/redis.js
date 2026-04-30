1963e940470eaae950e64eccbe7d93325700f479
Haitsams <harphypaw.09@gmail.com>
2026-04-30 16:19:14 +0700

    db

---
+++ src/database/redis.js
@@ -0,0 +1,15 @@
+const redis = require('redis');
+
+const client = redis.createClient({
+    socket: {
+        host: "127.0.0.1",
+        port: 6379,
+    },
+});
+
+
+client.on("error", (err) => console.error("Redis Client Error:", err));
+
+client.connect();
+
+module.exports = client;
